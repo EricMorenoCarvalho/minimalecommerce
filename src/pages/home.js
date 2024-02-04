d893a128184c2fcc17781a6df26905a754dc4d38
EricMorenoCarvalho <ericmorenowork@gmail.com>
2024-02-04 18:51:30 +0100

    Grid progress

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,17 +7,19 @@ import main3 from '../images/main3.jpg';
 const Home = () => {
   return (
     <div className='maincontent'>
-      <div className='image-container1'>
-        <img alt='photo1' src={main1} className='photo1' />
-        <p className='image-text1'>Everything</p>
-      </div>
-      <div className='image-container2'>
-        <img alt='photo2' src={main2} className='photo2' />
-        <p className='image-text2'>Chairs</p>
-      </div>
-      <div className='image-container3'>
-        <img alt='photo3' src={main3} className='photo3' />
-        <p className='image-text3'>Tables</p>
+      <div className='mainflex'>
+        <div className='image-container1'>
+          <img alt='photo1' src={main1} className='photo1' />
+          <p className='image-text text'>Everything</p>
+        </div>
+        <div className='image-container2'>
+          <img alt='photo2' src={main2} className='photo2' />
+          <p className='image-text text'>Chairs</p>
+        </div>
+        <div className='image-container3'>
+          <img alt='photo3' src={main3} className='photo3' />
+          <p className='image-text text'>Tables</p>
+        </div>
       </div>
     </div>
   );
